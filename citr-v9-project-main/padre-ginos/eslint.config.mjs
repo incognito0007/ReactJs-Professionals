1e0fbdb739439c00275e82c7d2454c722285463b
incognito0007 <anandaankit480@gmail.com>
2026-03-28 05:29:41 +0530

    Order, Cart, Pizza of the day

---
+++ citr-v9-project-main/padre-ginos/eslint.config.mjs
@@ -0,0 +1,33 @@
+import reactPlugin from "eslint-plugin-react";
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import { rules } from "@eslint/js/src/configs/eslint-all";
+
+/** @type {import ('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeature: { jsx: true },
+      },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
+  },
+  prettier,
+];
